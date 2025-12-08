7f35531c365d2ae05729f91678a6d94125ed04c1
Harsh Bhatt <harshbhatt7585@gmail.com>
2025-12-08 22:33:06 +0530

    fix mismatch shape

--- Transformers/model.py
+++ Transformers/model.py
@@ -2,17 +2,17 @@ import math
 import torch
 from torch import nn
 
+
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super(PositionalEncoding, self).__init__()
         
         pe = torch.zeros(max_len, d_model)
-
-        position = torch.arrange(0, max_len).unsqueeze(1)
+        position = torch.arange(0, max_len).unsqueeze(1)
 
         div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        ).view(1, -1, 1)
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        )
 
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
@@ -20,7 +20,8 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:, :x.size(1)]
+        x = x + self.pe[:x.size(1), :].unsqueeze(0)
+        return x
 
 
 class SelfAttention(nn.Module):
@@ -44,7 +45,7 @@ class SelfAttention(nn.Module):
 
 
 class TransformerBlock(nn.Module):
-    def __init__(self, d_model, n_heads):
+    def __init__(self, d_model):
         super(TransformerBlock, self).__init__()
         self.attn = SelfAttention(d_model)
         self.ln1 = nn.LayerNorm(d_model)
@@ -61,18 +62,16 @@ class TransformerBlock(nn.Module):
         return x
 
 class Transformer(nn.Module):
-    def __init__(self, d_model, n_heads, n_layers, n_vocab, max_len):
+    def __init__(self, d_model, n_layers, n_vocab, max_len):
         super(Transformer, self).__init__()
         self.d_model = d_model
-        self.n_heads = n_heads
         self.n_layers = n_layers
         self.n_vocab = n_vocab
         self.max_len = max_len
 
         self.embedding = nn.Embedding(n_vocab, d_model)
         self.positional_encoding = PositionalEncoding(d_model, max_len)
-        self.attention = SelfAttention(d_model)
-        self.transformer_blocks = nn.ModuleList([TransformerBlock(d_model, n_heads) for _ in range(n_layers)])
+        self.transformer_blocks = nn.ModuleList([TransformerBlock(d_model) for _ in range(n_layers)])
 
     def forward(self, x):
         x = self.embedding(x)
@@ -82,6 +81,13 @@ class Transformer(nn.Module):
         
         return x
 
+if __name__ == "__main__":
+    d_model = 512
+
+    model = Transformer(d_model, 6, 10000, 100)
+    x = torch.randint(0, 10000, (1, 100))
+    output = model(x)
+    print(output.shape)
 
 
     
