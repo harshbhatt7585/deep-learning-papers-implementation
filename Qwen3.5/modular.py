bc24053c313809b29b1093d364f65c5c08e7fd10
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-20 19:06:01 +0530

    reorder cache

--- Qwen3.5/modular.py
+++ Qwen3.5/modular.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Qwen3NextRMSNormGated(nn.Module):
     def __init__(self, hidden_size, eps=1e-6, **kwargs):
         super().__init__()
@@ -14,3 +17,59 @@ class Qwen3NextRMSNormGated(nn.Module):
         hidden_states = hidden_states * F.silu(gate.to(torch.float32))
 
         return hidden_states.to(input_dtypes)
+
+
+class Qwen3NextDynamicCache:
+    is_compileable = False
+
+
+    def __init__(self, config: Qwen3NextConfig):
+        super().__init__()
+        self.layer_types = config.layer_types
+        self.transformer_layer = [
+            i for i in range(config.num_hidden_layers) if self.layer_types[i] == "full_attention"
+        ]
+        self.last_linear_layer = len(self.layer_types) - 1 - self.layer_types[::-1].index("linear_attention")
+
+        self.conv_states = [None for _ in range(config.num_hidden_layers)]
+        self.recurrent_states = [None for _ in range(self.num_hidden_layers)]
+        self.key_cache = [None for _ in range(config.num_hidden_layers)]
+        self.value_cache = [None for _ in range(config.num_hidden_layers)]
+
+    
+    def __len__(self):
+        return len(self.layer_types)
+    
+
+    def update(
+        self,
+        key_states: torch.Tesnor,
+        value_states: torch.Tensor,
+        layer_idx: int,
+        cache_kwargs: dict[str, Any] | None = None,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+
+        if key_cache[layer_idx] is None:
+            self.key_cache[layer_idx] = key_states
+            self.value_cache[layer_idx] = value_states
+        else:
+            self.key_cache[layer_idx] = torch.cat([self.key_cache[layer_idx], key_states], dim=2)
+            self.value_cache[layer_idx] = torch.cat([self.value_cache[layer_idx], value_states], dim=2)
+        
+        return self.key_cache[layer_idx], self.value_cache[layer_idx]
+    
+    def reorder_cache(self, beam_idx: torch.LongTensor):
+        for layer_idx in range(len(self.key_cache)):
+            device = self.key_cache[layer_idx].device
+            beam_idx = beam_idx.to(device)
+            self.key_cache[layer_idx] = self.key_cache[layer_idx]
+            self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(0, beam_idx)
+        
+        if self.conv_states[layer_idx] is not None:
+            device = self.conv_states[layer_idx].device
+            beam_idx = beam_idx.to(device)
+            self.conv_states[layer_idx] = self.conv_states[layer_idx].index_select(0, beam_idx)
+            self.recurrent_states[layer_idx] = self.recurrent_states[layer_idx].index_select(0, beam_idx)
+            
+
+
