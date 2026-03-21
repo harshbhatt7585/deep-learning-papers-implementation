09fb7d5e3767e9d12fa5e5e333a64d7e9d424f07
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-22 03:26:58 +0530

    implement Qwen3_5GatedDeltaNet

--- Qwen3.5/modular.py
+++ Qwen3.5/modular.py
@@ -1,3 +1,4 @@
+from re import M
 from typing import Any, Optional
 import torch
 
@@ -393,8 +394,152 @@ class Qwen3_5GatedDeltaNet(nn.Module):
             self.num_v_heads,
             bias=True
         )
+    
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        cache_params: Qwen3_5DynamicsCache | None = None,
+        attention_mask: torch.Tensor | None = None
+    ):
+        hidden_states = apply_mask_to_padding_states(
+            hidden_states,
+            attention_mask
+        )
+        batch_size, seq_len, _ = hidden_states.shape
 
+        use_precomputed_states = cache_params is not None and cache_params.has_previous_state and seq_len == 1
+
+        if cache_params is not None:
+            conv_state = cache_params.conv_states[self.layer_idx]
+            recurrent_state = cache_params.recurrent_states[self.layer_idx]
         
+        mixed_qkv = self.in_proj_qkv(hidden_states)
+        mixed_qkv = mixed_qkv.transpose(1, 2)
+
+        z = self.in_proj_z(hidden_states)
+        z = z.reshape(batch_size, seq_len, -1, self.head_v_dim)
+
+        b = self.in_proj_b(hidden_states)
+        a = self.in_proj_a(hidden_states)
+
+        if use_precomputed_states:
+            mixed_qkv = self.casual_conv1d_update(
+                mixed_qkv,
+                conv_state,
+                self.conv1d.weight.squeeze(1),
+                self.conv1d.bias,
+                self.activation
+            )
+        else:
+            if cache_params is not None:
+                conv_state = F.pad(
+                    mixed_qkv,
+                    (self.conv_kernel_size - mixed_qkv.shape[-1], 0)
+                )
+                cache_params.conv_states[self.layer_idx] = conv_state
+            
+            if self.casual_conv1d_fn is not None:
+                mixed_qkv = self.casual_conv1d_fn(
+                    x=mixed_qkv,
+                    weight=self.conv1d.weight.squeeze(1),
+                    bias=self.conv1d.bias,
+                    activation=self.activation,
+                    seq_idx=None
+                )
+            else:
+                mixed_qkv = F.silu(
+                    self.conv1d(
+                        mixed_qkv[:, :, :seq_len]
+                    )
+                )
+
+        mixed_qkv = mixed_qkv.trasnpose(1, 2)
+        query, key, value = torch.split(
+            mixed_qkv,
+            [
+                self.key_dim,
+                self.key_dim,
+                self.value_dim
+            ],
+            dim=-1
+        )
+
+
+        query = query.reshape(
+            batch_size,
+            seq_len,
+            -1,
+            self.head_k_dim
+        )
+        key = key.reshape(
+            batch_size,
+            seq_len,
+            -1,
+            self.head_k_dim
+        )
+        value = value.reshape(
+            batch_size,
+            seq_len,
+            -1,
+            self.head_v_dim
+        )
+
+        beta = b.sigmoid()
+
+        g = -self.A_log.float().exp() * F.softplus(a.float() + self.dt_bias)
+        if self.num_v_heads // self.num_k_heads > 1:
+            query = query.repeat_interleave(
+                self.num_v_heads // self.num_k_heads, dim=2,
+            )
+            key = key.repeat_interleave(
+                self.num_v_heads // self.num_k_heads,
+                dim=2
+            )
+
+        if not use_precomputed_states:
+            core_attn_out, last_recurrent_state = self.chunk_gated_delta_rule(
+                query,
+                key,
+                value,
+                g=g,
+                beta=beta,
+                inital_state=None,
+                output_final_state=cache_params is not None,
+                use_qk_l2norm_in_kernel=True
+            )
+        
+        else:
+            core_attn_out, last_recurrent_state = self.recurrent_gated_delta_rule(
+                query,
+                key,
+                value,
+                g=g,
+                beta=beta,
+                inital_state=recurrent_state,
+                output_final_state=cache_params is not None,
+                use_qk_l2norm_in_kernel=True
+            )
+
+        if cache_params is not None:
+            cache_params.recurrent_states[self.layer_idx] = last_recurrent_state
+        
+        core_attn_out = core_attn_out.reshape(-1, self.head_v_dim)
+        z = z.reshape(
+            -1,
+            self.head_v_dim
+        )
+        core_attn_out = self.norm(core_attn_out, z)
+        core_attn_out = core_attn_out.reshape(
+            batch_size,
+            seq_len,
+            -1
+        )
+
+        output = self.out_proj(core_attn_out)
+        return output
+
+
+
 
     
 
