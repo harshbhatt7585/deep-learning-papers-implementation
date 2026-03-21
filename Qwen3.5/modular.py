26f674bf2dc6787f8ef9a06210bf4f74d6e6e92d
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-22 03:29:46 +0530

    rotate half

--- Qwen3.5/modular.py
+++ Qwen3.5/modular.py
@@ -540,6 +540,15 @@ class Qwen3_5GatedDeltaNet(nn.Module):
 
 
 
+def rotate_half(x):
+    x1 = x[..., x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+    return torch.cat(
+        (-x2, x1), dim=-1
+    )
+    
+
+
 
     
 
