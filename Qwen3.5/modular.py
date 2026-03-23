c024ca1572b1726e602ee9e1f9a8cca24c95e7dc
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-23 22:18:46 +0530

    implement Qwen vision block

--- Qwen3.5/modular.py
+++ Qwen3.5/modular.py
@@ -845,4 +845,34 @@ class Qwen3_5DecoderLayer(GradientCheckpointingLayer):
         return hidden_states
 
 
+class Qwen3_5VisionBlock(GradientCheckpointingLayer):
+    def __init__(self, config, attn_implementation: str = "sdpa") -> None:
+        super().__init__()
+        self.norm1 = nn.LayerNorm(config.hidden_size, eps=1e-6)
+        self.norm2 = nn.LayerNorm(config.hidden_size, eps=1e-6)
+
+        self.attn = Qwen3_5VisionBlock(config=config)
+        self.mlp = Qwen3_5VisionBlock(config=config)
+
+    
+    @auto_docstring
+    def forward(
+        self,
+        hidden_states:  torch.Tensor,
+        cu_seqlens: torch.Tensor,
+        rotary_pos_emb: torch.Tensor | None = None,
+        postional_embeddings: tuple[torch.Tensor, torch.Tensor] | None = None
+    ) -> torch.Tensor:
+        hidden_states = hidden_states + self.attn(
+            self.norm1(hidden_states),
+            cu_seqlens=cu_seqlens,
+            rotary_pos_emb=rotary_pos_emb,
+            postional_embeddings=postional_embeddings,
+            **kwargs
+        )
+        hidden_states = hidden_states + self.mlp(self.norm2(hidden_states))
+        return hidden_states
+
+
+
     
