acfc8b77af6d0832a2e2779cfe9964d0880ad57a
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-27 22:51:18 +0530

    implement mlp

---
+++ Qwen3.5/mlp.py
@@ -0,0 +1,17 @@
+
+from torch import nn
+from .utils import ACT2FN, nn
+
+class Qwen3MLP(nn.Module):
+    def __init__(self, config)
+    super().__init__()
+    self.gate_proj = nn.Linear(config.hidden_size, config.intermediate_size, bias=True)
+    self.up_proj = nn.Linear(config.hidden_size, config.intermediate_size, bias=True)
+    self.down_proj = nn.Linear(config.intermediate_size, config.hidden_size, bias=True)
+
+    self.act_gn = ACT2FN(config.hidden_act)
+
+
+def forward(self, x):
+    return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+    
