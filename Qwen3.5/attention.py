14524d55cbe7ac27fb35073bb7fcca640ee11e30
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-27 19:26:04 +0530

    implement Qwen35Attention

--- Qwen3.5/attention.py
+++ Qwen3.5/attention.py
@@ -1,7 +1,9 @@
+import torch
 from torch import nn
 from .norm import Qwen35RMSNorm
 from .rope import apply_rotary_pos_emd
 from .utils import repeat_kv
+
 
 
 class Qwen35Attention(nn.Module):
@@ -44,3 +46,38 @@ class Qwen35Attention(nn.Module):
             self.head_dim,
             eps=config.rms_norm_eps
         )
+
+    
+    def forward(
+        self,
+        hidden_states,
+        positon_embeddings,
+        attention_mask,
+        past_key_values=None
+    ):
+        input_shape = hidden_states.shape[:-1]
+        hidden_shape = (*input_shape, -1, self.head_dim)
+
+        query_states, gate = torch.chunk(
+            self.q_proj(hidden_states).view(*input_shape, -1, self.head_dim * 2),
+            2,
+            dim=-1
+        )
+        gate= gate.reshape(*input_shape, -1)
+
+        query_states = self.q_norm(query_states.view(hidden_shape)).transpose(1, 2)
+        key_states = self.k_norm(self.k_proj(hidden_states).view(hidden_shape)).trasnpose(1, 2)
+        value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
+
+        cos, sin = positon_embeddings
+        query_states, key_states = apply_rotary_pos_emd(query_states,key_states, cos, sin)
+
+        if past_key_values is not None:
+            attn_weights = attn_weights + attention_mask
+        
+        attn_weights = torch.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        attn_output = torch.matmul(attn_weights, value_states)
+        attn_output = attn_output.transpose(1, 2).contagious()
+        attn_output = attn_output.reshape(*input_shape, -1)
+        attn_output = attn_output * torch.sigmoid(gate)
+        return attn_output
